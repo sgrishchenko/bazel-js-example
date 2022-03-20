1ff0bbef63fe5a583dfe786c4a90007f83421c4f
sergeigrishchenko <s.i.grishchenko@gmail.com>
2022-03-20 10:45:29 +0300

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+
+export default {
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+        replace({
+            preventAssignment: true,
+            values: {
+                'process.env.NODE_ENV': JSON.stringify('production'),
+            }
+        })
+    ]
+}
